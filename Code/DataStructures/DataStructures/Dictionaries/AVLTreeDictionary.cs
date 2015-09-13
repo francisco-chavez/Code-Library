4c706040255996119556c03e96f4ee1723ae80c9
Francisco Chavez-Tejeda <francisco.chavez.509@outlook.com>
2015-09-13 16:13:46 -0700

    Created IDictionary interface. Created AVLTreeDictionary stub class.

---
+++ Code/DataStructures/DataStructures/Dictionaries/AVLTreeDictionary.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Unvi.DataStructures.Dictionaries
+{
+	public class AVLTreeDictionary<TKey, TValue>
+		: IDictionary<TKey, TValue> where TKey : IComparable
+	{
+		#region Properties
+		public TValue this[TKey key]
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+
+			set
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public int Count
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+		#endregion
+
+
+		#region Public Methods
+		public void Add(TKey key, TValue value)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Remove(TKey key)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Clear()
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool ContainsKey(TKey key)
+		{
+			throw new NotImplementedException();
+		}
+
+
+		public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+		{
+			throw new NotImplementedException();
+		}
+		#endregion
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Code/DataStructures/DataStructures/IDictionary.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unvi.DataStructures
+{
+	public interface IDictionary<TKey, TValue>
+		: IEnumerable<KeyValuePair<TKey, TValue>>
+	{
+		/// <summary>
+		/// Gets or sets the entry under the given key.
+		/// </summary>
+		TValue this[TKey key] { get; set; }
+		/// <summary>
+		/// Gets the current number of entries.
+		/// </summary>
+		int Count { get; }
+
+		/// <summary>
+		/// Adds given value as a new entry under the given key.
+		/// </summary>
+		void Add(TKey key, TValue value);
+		/// <summary>
+		/// Removes the entry under the given key.
+		/// </summary>
+		void Remove(TKey key);
+		/// <summary>
+		/// Checks to see if there's an entry under the given key.
+		/// </summary>
+		bool ContainsKey(TKey key);
+		/// <summary>
+		/// Removes all entries.
+		/// </summary>
+		void Clear();
+	}
+}
