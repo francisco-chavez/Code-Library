db607a49e6a1710507d179a77996eb1477ed931d
Francisco Chavez-Tejeda <francisco.chavez.509@outlook.com>
2015-09-12 18:57:59 -0700

    Started work an an ISet<T> implementation that's backed ay an AVL Tree.

---
+++ Code/DataStructures/DataStructures/Sets/AVLTreeSet.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Unvi.DataStructures.Sets
+{
+	public class AVLTreeSet<T>
+		: ISet<T>
+	{
+		#region Attributes
+		private int _count;
+		#endregion
+
+
+		#region Properties
+		public int Count
+		{
+			get { return _count; }
+			private set { _count = value; }
+		}
+		#endregion
+
+
+		#region Constructors
+		public AVLTreeSet()
+		{
+			Count = 0;
+		}
+		#endregion
+
+
+		#region Public Methods
+
+		#region Set Manipulation
+		public void Add(T value)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Remove(T value)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool Contains(T value)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Clear()
+		{
+			throw new NotImplementedException();
+		}
+		#endregion
+
+
+		#region Set Meta Data
+		public bool IsSubsetOf(ISet<T> otherSet)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool IsSupersetOf(ISet<T> otherSet)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool IsProperSubsetOf(ISet<T> otherSet)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool IsProperSupersetOf(ISet<T> otherSet)
+		{
+			throw new NotImplementedException();
+		}
+		#endregion
+
+
+		#region Set Creation
+		public ISet<T> Instersection(ISet<T> otherSet)
+		{
+			throw new NotImplementedException();
+		}
+
+		public ISet<T> Union(ISet<T> otherSet)
+		{
+			throw new NotImplementedException();
+		}
+
+		public ISet<T> Complement(ISet<T> otherSet)
+		{
+			throw new NotImplementedException();
+		}
+
+		public ISet<T> SymmetricDifference(ISet<T> otherSet)
+		{
+			throw new NotImplementedException();
+		}
+		#endregion
+
+
+		public IEnumerator<T> GetEnumerator()
+		{
+			throw new NotImplementedException();
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			throw new NotImplementedException();
+		}
+		#endregion
+	}
+}
