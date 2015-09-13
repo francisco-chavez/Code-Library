f0ae9f731d440ff84190e84984d0acc442fdef12
Francisco Chavez-Tejeda <francisco.chavez.509@outlook.com>
2015-09-13 16:31:59 -0700

    Implemented Node class for AVLTreeDictionary.

--- Code/DataStructures/DataStructures/Dictionaries/AVLTreeDictionary.cs
+++ Code/DataStructures/DataStructures/Dictionaries/AVLTreeDictionary.cs
@@ -63,9 +63,64 @@ namespace Unvi.DataStructures.Dictionaries
 		}
 		#endregion
 
+
+		#region Helper Classes
 		IEnumerator IEnumerable.GetEnumerator()
 		{
 			throw new NotImplementedException();
 		}
+		#endregion
+
+
+		#region Inner Classes
+		private class Node
+		{
+			#region Properties
+			public Node		Parent	{ get; set; }
+			public Node		Left	{ get; set; }
+			public Node		Right	{ get; set; }
+
+			public TKey		Key		{ get; private set; }
+			public TValue	Value	{ get; set; }
+
+			public int		Height { get; private set; }
+			public int		Balance
+			{
+				get
+				{
+					int left = Left != null ? Left.Height : 0;
+					int right = Right != null ? Right.Height : 0;
+
+					return left - right;
+				}
+			}
+			#endregion
+
+
+			#region Constructors
+			public Node(TKey key, TValue value)
+			{
+				Key		= key;
+				Value	= value;
+				Height	= 1;
+
+				Parent	= null;
+				Left	= null;
+				Right	= null;
+			}
+			#endregion
+
+
+			#region Methods
+			public void UpdateHeight()
+			{
+				int left  = Left  != null ? Left.Height  : 0;
+				int right = Right != null ? Right.Height : 0;
+
+				Height = 1 + Math.Max(left, right);
+			}
+			#endregion
+		}
+		#endregion
 	}
 }
