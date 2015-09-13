bf47edcfcbed526b9fbf721019869e4d9219a49f
Francisco Chavez-Tejeda <francisco.chavez.509@outlook.com>
2015-09-12 20:07:14 -0700

    Implementing Add method for AVLTreeSet class.
    I've implemented a few AVL Trees from scratch in the last two months, so
    I have a good idea of which helper classes will be needed. Because of
    this, I can get a good start on those methods without refactoring the
    code from their calling methods.

--- Code/DataStructures/DataStructures/Sets/AVLTreeSet.cs
+++ Code/DataStructures/DataStructures/Sets/AVLTreeSet.cs
@@ -9,10 +9,11 @@ using System.Threading.Tasks;
 namespace Unvi.DataStructures.Sets
 {
 	public class AVLTreeSet<T>
-		: ISet<T>
+		: ISet<T> where T : IComparable<T>
 	{
 		#region Attributes
-		private int _count;
+		private int		_count;
+		private Node	_root;
 		#endregion
 
 
@@ -22,13 +23,34 @@ namespace Unvi.DataStructures.Sets
 			get { return _count; }
 			private set { _count = value; }
 		}
+
+		public int Height
+		{
+			get
+			{
+				if(Count == 0)
+					return 0;
+				else
+					return _root.Height;
+			}
+		}
 		#endregion
 
 
 		#region Constructors
 		public AVLTreeSet()
 		{
+			_root = null;
 			Count = 0;
+		}
+
+		/// <summary>
+		/// Destructor for AVLTreeSet. This isn't really needed, but it should break
+		/// the tree down a bit faster making it easier on the GC.
+		/// </summary>
+		~AVLTreeSet()
+		{
+			Clear();
 		}
 		#endregion
 
@@ -38,7 +60,31 @@ namespace Unvi.DataStructures.Sets
 		#region Set Manipulation
 		public void Add(T value)
 		{
-			throw new NotImplementedException();
+			// Null is not a valid value
+			if(value == null)
+				return;
+
+			// The value is already present
+			if (Contains(value))
+				return;
+
+			if (_root == null)
+			{
+				_root = new Node(value);
+				Count++;
+				return;
+			}
+
+			var parent = GetParentNode(value);
+			var newNode = new Node(value) { Parent = parent };
+
+			if(newNode.Data.CompareTo(parent.Data) < 0)
+				parent.Left = newNode;
+			else
+				parent.Right = newNode;
+
+			Count++;
+			RebalanceTree(newNode);
 		}
 
 		public void Remove(T value)
@@ -116,15 +162,59 @@ namespace Unvi.DataStructures.Sets
 		#endregion
 
 
+		#region Helper Methods
+		private Node GetParentNode(T value)
+		{
+			throw new NotImplementedException();
+		}
+
+		private void RebalanceTree(Node newNode)
+		{
+			throw new NotImplementedException();
+		}
+		#endregion
+
+
 		#region Helper Classes
 		private class Node
 		{
+			#region Properties
 			public Node Parent	{ get; set; }
 			public Node Left	{ get; set; }
 			public Node Right	{ get; set; }
-			public int	Height	{ get; set; }
 
 			public T	Data	{ get; set; }
+
+			public int	Height	{ get; set; }
+			public int	Balance
+			{
+				get
+				{
+					int leftHeight  = Left  == null ? 0 : Left.Height;
+					int rightHeight = Right == null ? 0 : Left.Height;
+
+					return leftHeight - rightHeight;
+				}
+			}
+			#endregion
+
+
+			#region Constructors
+			public Node()
+				: this(Activator.CreateInstance<T>())
+			{
+			}
+
+			public Node(T value)
+			{
+				this.Parent = null;
+				this.Left	= null;
+				this.Right	= null;
+				this.Height = 1;
+
+				this.Data	= value;
+			}
+			#endregion
 		}
 		#endregion
 	}
