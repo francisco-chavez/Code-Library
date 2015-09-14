bddd221dfcd2baff9b2056b0f8ea986ffccd4c46
Francisco Chavez-Tejeda <francisco.chavez.509@outlook.com>
2015-09-13 17:12:32 -0700

    Started working on IHeap interface.

---
+++ Code/DataStructures/DataStructures/IHeap.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Unvi.DataStructures
+{
+	public interface IHeap<T>
+		where T : IComparable<T>
+	{
+		HeapType	HeapType	{ get; }
+		T			Peek		{ get; }
+		int			Count		{ get; }
+		bool		IsEmpty		{ get; }
+
+		void Push(T value);
+		T Pop();
+	}
+}
