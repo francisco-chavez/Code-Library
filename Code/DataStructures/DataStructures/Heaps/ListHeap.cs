be00dd45157ea4eb1a6a196b50fe332c0e7a50be
Francisco Chavez-Tejeda <francisco.chavez.509@outlook.com>
2015-09-14 18:24:58 -0700

    Finished initial implementation of ListHeap<T>.

--- Code/DataStructures/DataStructures/Heaps/ListHeap.cs
+++ Code/DataStructures/DataStructures/Heaps/ListHeap.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Unvi.DataStructures.Heaps
 {
 	public class ListHeap<T>
@@ -39,8 +40,14 @@ namespace Unvi.DataStructures.Heaps
 
 		public ListHeap(HeapType heapType, IEnumerable<T> data)
 		{
-			_data = data != null ? new List<T>(data) : new List<T>(10);
-			throw new NotImplementedException();
+			if (data == null)
+				_data = new List<T>(10);
+			else if (data.Any(datum => { return datum == null; }))
+				throw new NullReferenceException("Given data contains null values");
+			else
+				_data = data != null ? new List<T>(data) : new List<T>(10);
+
+			Heapify();
 		}
 		#endregion
 
@@ -62,57 +69,7 @@ namespace Unvi.DataStructures.Heaps
 			_data[0] = _data[Count - 1];
 			_data.RemoveAt(Count - 1);
 
-			int current = 0;
-			int left = LeftChildIndex(current);
-			int right = RightChildIndex(current);
-			bool keepGoing = true;
-
-			while (keepGoing)
-			{
-				if ((current >= Count - 1) || (left >= Count))
-				{
-					keepGoing = false;
-					continue;
-				}
-
-				int child = left;
-				// if has right child
-				if (right >= Count)
-				{
-					switch (HeapType)
-					{
-					case HeapType.Max:
-						child = _data[left].CompareTo(_data[right]) < 0 ? right : left;
-						break;
-					case HeapType.Min:
-						child = _data[left].CompareTo(_data[right]) > 0 ? right : left;
-						break;
-					}
-				}
-
-				switch (HeapType)
-				{
-				case HeapType.Max:
-					if (_data[current].CompareTo(_data[child]) >= 0)
-						keepGoing = false;
-					break;
-
-				case HeapType.Min:
-					if (_data[current].CompareTo(_data[child]) <= 0)
-						keepGoing = false;
-					break;
-				}
-
-				if (keepGoing)
-				{
-					T datum = _data[current];
-					_data[current] = _data[child];
-					_data[child] = datum;
-					current = child;
-					left = LeftChildIndex(current);
-					right = RightChildIndex(current);
-				}
-			}
+			FixHeap(0);
 
 			return result;
 		}
@@ -122,51 +79,142 @@ namespace Unvi.DataStructures.Heaps
 			if(value == null)
 				throw new ArgumentNullException("value");
 
-			int index = _data.Count;
+			int child = _data.Count;
+			int parent = FindParent(child);
 			_data.Add(value);
 
-			while (!IsValidChild(index) && index > 0)
+			while (!HeapValidFor(parent, child))
 			{
-				int parentIndex = GetParentIndex(index);
-				_data[index] = _data[parentIndex];
-				_data[parentIndex] = value;
-				index = parentIndex;
+				_data[child] = _data[parent];
+				_data[parent] = value;
+				child = parent;
+				parent = FindParent(child);
 			}
 		}
 		#endregion
 
 
 		#region Helper Methods
-		private bool IsValidChild(int index)
+		private void Heapify()
 		{
-			if (index == 0)
-				return true;
+			// Nothing to do.
+			if(Count == 0 || Count == 1)
+				return;
 
-			int parent = GetParentIndex(index);
-			switch (HeapType)
+			// Lets skip all (or most) of the leaf nodes. These guys
+			// already meet the heap property by being leafs.
+			for (int i = Count / 2; i >= 0; i--)
+				FixHeap(i);
+		}
+
+		/// <summary>
+		/// Starting from a parent, this method will move the parent value down
+		/// the tree until the value falls into a node that meets the heap property
+		/// with said node's children.
+		/// </summary>
+		/// <remarks>
+		/// In order for this method to work properly, both child trees of the given
+		/// parent must be proper heaps.
+		/// </remarks>
+		private void FixHeap(int parent)
+		{
+			while (!HeapValidFor(parent))
 			{
-			case HeapType.Max:
-				return _data[index].CompareTo(_data[parent]) <= 0;
+				int child = FindLeft(parent);
+				int right = FindRight(parent);
 
-			case HeapType.Min:
-				return _data[index].CompareTo(_data[parent]) >= 0;
+				// if has right child
+				if (right < Count)
+				{
+					// One or two children are breaking the heap property. If we switch with
+					// the child that breaks it the most, then the heap property will be 
+					// restored at this point in the tree.
+					switch (HeapType)
+					{
+					case HeapType.Max:
+						child = _data[child].CompareTo(_data[right]) < 0 ? right : child;
+						break;
+					case HeapType.Min:
+						child = _data[child].CompareTo(_data[right]) > 0 ? right : child;
+						break;
+					}
+				}
 
-			default: // needed to compile
-				return false;
+				T datum = _data[parent];
+				_data[parent] = _data[child];
+				_data[child] = datum;
+				parent = child;
 			}
 		}
 
-		private int GetParentIndex(int childIndex)
+
+		/// <summary>
+		/// The method tells us if the heap property is valid for the given parent
+		/// and both of its children.
+		/// </summary>
+		private bool HeapValidFor(int parent)
+		{
+			int left = FindLeft(parent);
+			int right = FindRight(parent);
+
+			if(!HeapValidFor(parent, left))
+				return false;
+			if (!HeapValidFor(parent, right))
+				return false;
+
+			return true;
+		}
+
+		/// <summary>
+		/// The method tells us if the values in the given parent index and the given
+		/// child index fulfill the heap property for the HeapType.
+		/// </summary>
+		private bool HeapValidFor(int parent, int child)
+		{
+			// Safety checks
+			if (parent >= Count)
+				return true;
+			if (child >= Count)
+				return true;
+			if (parent < 0)
+				return true;
+			if (child < 0)
+				return true;
+
+			switch (this.HeapType)
+			{
+			case HeapType.Max:
+				// Parent value is smaller than child value.
+				if (_data[parent].CompareTo(_data[child]) < 0)
+					return false;
+				break;
+
+			case HeapType.Min:
+				// Parent value is larger than child value.
+				if (_data[parent].CompareTo(_data[child]) > 0)
+					return false;
+				break;
+
+			default:
+				// How did you even get here?
+				throw new InvalidOperationException(string.Format("HeapType set to unsuported type '{0}'.", this.HeapType));
+			}
+
+			return true;
+		}
+
+
+		private int FindParent(int childIndex)
 		{
 			return (childIndex - 1) / 2;
 		}
 
-		private int LeftChildIndex(int parentIndex)
+		private int FindLeft(int parentIndex)
 		{
 			return parentIndex * 2 + 1;
 		}
 
-		private int RightChildIndex(int parentIndex)
+		private int FindRight(int parentIndex)
 		{
 			return parentIndex * 2 + 2;
 		}
