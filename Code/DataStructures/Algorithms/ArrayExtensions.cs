889c0f20078c9adf55e32ea3a0874cb2bc902aa2
Francisco Chavez-Tejeda <francisco.chavez.509@outlook.com>
2015-09-20 19:21:50 -0700

    Added extension methods for turning sorting the elements in an array into a min/max heap.

---
+++ Code/DataStructures/Algorithms/ArrayExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Unvi.Algorithms
+{
+	public static class ArrayExtensions
+	{
+		public static void SwapValues<T>(this T[] data, int indexA, int indexB)
+		{
+			if (data == null)
+				throw new ArgumentNullException("There is no array to work with.");
+
+			if (indexA < 0 || data.Length <= indexA)
+				throw new ArgumentOutOfRangeException("indexA");
+
+			if (indexB < 0 || data.Length <= indexB)
+				throw new ArgumentOutOfRangeException("indexB");
+
+			if (indexA == indexB)
+				return;
+
+			T datum = data[indexA];
+			data[indexA] = data[indexB];
+			data[indexB] = datum;
+		}
+	}
+}
---
+++ Code/DataStructures/Algorithms/Heaps/ArrayExtensions.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Linq;
+
+
+namespace Unvi.Algorithms.Heaps
+{
+	public static class ArrayExtensions
+	{
+		#region Public Extension Methods
+		/// <summary>
+		/// This method will sort the elements in the array into a minimum heap, 
+		/// with the smallest value at index 0.
+		/// </summary>
+		public static void HeapifyMin<T>(this T[] data)
+			where T : IComparable, IComparable<T>
+		{
+			if (data == null)
+				throw new ArgumentNullException("There is no array to work on.");
+
+			if (data.Length < 2)
+				return;
+
+			if (data.Any(datum => { return datum == null; }))
+				throw new ArgumentException("There is one or more null entries in the array.");
+
+			for (int i = data.Length / 2; i > -1; i--)
+				data.FixMinHeap(i);
+		}
+
+		/// <summary>
+		/// This method will sort the elements in the array into a maximum heap,
+		/// with the largest value at index 0.
+		/// </summary>
+		public static void HeapifyMax<T>(this T[] data)
+			where T : IComparable, IComparable<T>
+		{
+			if (data == null)
+				throw new ArgumentNullException("There is no array to work on.");
+
+			if (data.Length < 2)
+				return;
+
+			if (data.Any(datum => { return datum == null; }))
+				throw new ArgumentException("There is one or more null entries in the array.");
+
+			for (int i = data.Length / 2; i > -1; i--)
+				data.FixMaxHeap(i);
+		}
+		#endregion
+
+
+		#region Helper Methods
+		private static void FixMinHeap<T>(this T[] data, int index)
+			where T : IComparable, IComparable<T>
+		{
+			if (data.IsMinHeap(index))
+				return;
+
+			int left = (index * 2) + 1;
+			int right = (index * 2) + 2;
+
+			int min = left;
+
+			if (right < data.Length && data[right].CompareTo(data[left]) < 0)
+				min = right;
+
+			data.SwapValues(index, min);
+		}
+
+		private static void FixMaxHeap<T>(this T[] data, int index)
+			where T : IComparable, IComparable<T>
+		{
+			if (data.IsMaxHeap(index))
+				return;
+
+			int left = (index * 2) + 1;
+			int right = (index * 2) + 2;
+
+			int max = left;
+
+			if (right < data.Length && data[right].CompareTo(data[left]) > 0)
+				max = right;
+
+			data.SwapValues(index, max);
+		}
+
+		private static bool IsMinHeap<T>(this T[] data, int index)
+			where T : IComparable, IComparable<T>
+		{
+			int left = (index  * 2) + 1;
+			int right = (index * 2) + 2;
+
+			if (left >= data.Length)
+				return true;
+
+			if (data[index].CompareTo(data[left]) > 0)
+				return false;
+
+			if (right < data.Length)
+				return data[index].CompareTo(data[right]) <= 0;
+			
+			return true;
+		}
+
+		private static bool IsMaxHeap<T>(this T[] data, int index)
+			where T : IComparable, IComparable<T>
+		{
+			int left = (index * 2) + 1;
+			int right = (index * 2) + 2;
+
+			if (left >= data.Length)
+				return true;
+
+			if (data[index].CompareTo(data[left]) < 0)
+				return false;
+
+			if (right < data.Length)
+				return data[index].CompareTo(data[right]) >= 0;
+
+			return true;
+		}
+		#endregion
+	}
+}
