b5be8e09d674fc30483e50a86453cb53e5b2e392
Francisco Chavez-Tejeda <francisco.chavez.509@outlook.com>
2015-11-20 19:47:38 -0800

    Started to convert the AVLTreeDictionary class to use the IDictionary interface provided by the .NET framework.
    Using the interface provided by the .NET framework, should make my class
    more compatible with other programs that already use the standard
    Dictionary class that is provided by the .NET framework.

--- Code/DataStructures/DataStructures/Dictionaries/AVLTreeDictionary.cs
+++ Code/DataStructures/DataStructures/Dictionaries/AVLTreeDictionary.cs
@@ -11,36 +11,44 @@ namespace Unvi.DataStructures.Dictionaries
 		: IDictionary<TKey, TValue> where TKey : IComparable<TKey>
 	{
 		#region Attributes
-		private Node _root;
+		private Node	_root;
 		#endregion
 
 
 		#region Properties
-		/// <summary>
-		/// Gets the current number of entries.
-		/// </summary>
 		public int Count { get; private set; }
 
 		/// <summary>
-		/// Gets or sets the entry under the given key.
+		/// Gets or sets the element with the specified key.
 		/// </summary>
+		/// <param name="key">The key of the element to get or set.</param>
+		/// <returns>The element with the specified key.</returns>
 		public TValue this[TKey key]
 		{
 			get
 			{
+				if (key == null)
+					throw new ArgumentNullException();
+
 				if (!ContainsKey(key))
-					throw new IndexOutOfRangeException("No entry was found under the given key.");
+					throw new KeyNotFoundException();
 
 				var parent = GetParent(key);
 				if (parent == null)
 					return _root.Value;
 
 				return key.CompareTo(parent.Key) < 0 ? parent.Left.Value : parent.Right.Value;
-			}
+			}	// End get
 			set
 			{
+				if (IsReadOnly)
+					throw new NotSupportedException("Dictionary is read-only.");
+
+				if (key == null)
+					throw new ArgumentNullException("key");
+
 				if (!ContainsKey(key))
-					throw new IndexOutOfRangeException("No entry was found under the given key.");
+					throw new KeyNotFoundException();
 
 				var parent = GetParent(key);
 				if (parent == null)
@@ -48,8 +56,9 @@ namespace Unvi.DataStructures.Dictionaries
 
 				Node n = key.CompareTo(parent.Key) < 0 ? parent.Left : parent.Right;
 				n.Value = value;
-			}
-		}
+			}	// End set
+		}	// End [] property.
+
 		#endregion
 
 
@@ -67,16 +76,23 @@ namespace Unvi.DataStructures.Dictionaries
 		#endregion
 
 
-		#region Public Methods
+		#region Methods
+
 		/// <summary>
-		/// Adds given value as a new entry under the given key.
+		/// Adds an element with the provided key and value to the AVLTreeDictionary&lt;Tkey, TValue&gt;.
 		/// </summary>
+		/// <param name="key">The object to use as the key of the element to add.</param>
+		/// <param name="value">The object to use as the value of the element to add.</param>
 		public void Add(TKey key, TValue value)
 		{
-			if(key == null)
-				throw new ArgumentNullException("key");
+			if (IsReadOnly)
+				throw new NotSupportedException("IDictionary is in read-only mode.");
+
+			if (key == null)
+				throw new ArgumentNullException("Key is null.");
+
 			if (ContainsKey(key))
-				throw new ArgumentException(string.Format("Key \"{0}\" is already contained in {1}.", key, this.GetType()));
+				throw new ArgumentException("An element with the same key already exists in the IDictionary.");
 
 			// Base case, the tree is empty, so we don't have to worry
 			// about keeping it balanced or searching for a location to 
@@ -101,206 +117,86 @@ namespace Unvi.DataStructures.Dictionaries
 
 			Count++;
 			RebalanceTree(node);
+		}	// End +Add(key: TKey, value: TValue)
+
+		/// <summary>
+		/// Adds an element with the provided key and value to the AVLTreeDictionary&lt;Tkey, TValue&gt;.
+		/// </summary>
+		public void Add(KeyValuePair<TKey, TValue> item)
+		{
+			Add(item.Key, item.Value);
 		}
 
 		/// <summary>
-		/// Removes the entry under the given key.
+		/// Removes the element with the spedified key from the AVLTreeDictionary&lt;TKey, TValue&gt;.
 		/// </summary>
-		public void Remove(TKey key)
+		/// <param name="key">The key of the element to remove.</param>
+		/// <returns>
+		/// True if the element is successfully removed; otherwise, false. This method also returns false
+		/// if the key was not found.
+		/// </returns>
+		public bool Remove(TKey key)
 		{
-			if(key == null)
-				throw new ArgumentNullException("key");
+			if (key == null)
+				throw new ArgumentNullException();
 
-			if (Count == 0 || !ContainsKey(key))
-				throw new ArgumentOutOfRangeException("key", "There is no entry for the given key value in the dictionary.");
+			if (IsReadOnly)
+				throw new NotSupportedException("The IDictionary<TKey> is read-only.");
 
-			// There's no need for anything complicated if we only 
-			// have the one node in the tree.
+			// Key not found.
+			if (!ContainsKey(key))
+				return false;
+
+			// Edge Case: It's the only item.
 			if (Count == 1)
 			{
 				_root = null;
 				Count = 0;
-				return;
+				return true;
 			}
 
-			Node parent;
-			Node node;
-			Node replacement;
-
-			parent = GetParent(key);
-			if (parent == null)
-				node = _root;
-			else if (key.CompareTo(parent.Key) < 0)
-				node = parent.Left;
-			else
-				node = parent.Right;
-
-			// Special case. The replacement node should be the rightmost
-			// node of the left sub-tree of the node that will be removed
-			// from the tree. If we don't have a replacement node on the
-			// left substree, we will use the first node on the right 
-			// subtree.
-			if (node.Left == null)
-			{
-				replacement = node.Right;
-
-				if (parent == null)
-					_root = replacement;
-				else if (key.CompareTo(parent.Key) < 0)
-					parent.Left = replacement;
-				else
-					parent.Right = replacement;
-
-				if (replacement != null)
-					replacement.Parent = parent;
-
-				node.Parent = null;
-				node.Right = null;
-				Count--;
-
-				// This should really be replacement.Parent but the replacment could
-				// be null and calling .Parent on a null node will throw an excpetion.
-				// This works just fine, and we'll just end up checking one more level
-				// of the tree when we rebalance the thing.
-				RebalanceTree(replacement);
-				return;
-			}
-
-			replacement = node.Left;
-			while (replacement.Right != null)
-				replacement = replacement.Right;
-
-			Node rebalancePoint = null;
-
-			if (replacement == node.Left)
-			{
-				rebalancePoint = replacement;
-			}
-			else
-			{
-				rebalancePoint = replacement.Parent;
-				// The replacment was the right most child of the sub-tree,
-				// so we know that it resides on the right side of its parent.
-				rebalancePoint.Right = replacement.Left;
-				if (rebalancePoint.Right != null)
-					rebalancePoint.Right.Parent = rebalancePoint;
-
-				replacement.Left = node.Left;
-				replacement.Left.Parent = replacement;
-			}
-
-			replacement.Right = node.Right;
-			if (replacement.Right != null)
-				replacement.Right.Parent = replacement;
-
-			replacement.Parent = parent;
-			if (parent == null)
-				_root = replacement;
-			else if (key.CompareTo(parent.Key) < 0)
-				parent.Left = replacement;
-			else
-				parent.Right = replacement;
-
-			Count--;
+			Node rebalancePoint = RemoveNode(key);
 			RebalanceTree(rebalancePoint);
-			node.Left = null;
-			node.Right = null;
-			node.Parent = null;
-		}
+			return true;
+		}	// End +Remove(key: TKey): bool
 
 		/// <summary>
-		/// Removes all entries.
+		/// Removes the first (and only) occurrence of the item. It a different value
+		/// is held under this key, nothing will be removed.
+		/// </summary>
+		public bool Remove(KeyValuePair<TKey, TValue> item)
+		{
+			if (!this.ContainsKey(item.Key))
+				return false;
+
+			var value = this[item.Key];
+			if (!item.Value.Equals(value))
+				return false;
+
+			return this.Remove(item.Key);
+		}	// End +Remove(item: KeyValuePair<TKey, TValue>): bool
+
+		/// <summary>
+		/// Clears out all entries in the AVLTreeDictionary&lt;Tkey, TValue&gt;.
 		/// </summary>
 		public void Clear()
 		{
 			Clear(_root);
 			_root = null;
 			Count = 0;
-		}
+		}	// End +Clear()
 
-		/// <summary>
-		/// Checks to see if there's an entry under the given key.
-		/// </summary>
-		public bool ContainsKey(TKey key)
-		{
-			if(key == null)
-				return false;
-
-			if (Count == 0)
-				return false;
-
-			var parent = GetParent(key);
-
-			if (parent == null)
-				return _root.Key.CompareTo(key) == 0;
-
-			var node = key.CompareTo(parent.Key) < 0 ? parent.Left : parent.Right;
-
-			return node != null;
-		}
-
-		public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
-		{
-			if (_root == null)
-				yield break;
-
-			var stack = new Node[_root.Height];
-			int loc = 0;
-			var current = _root;
-
-			FillStack(stack, ref loc, current);
-
-			while (loc > 0)
-			{
-				current = stack[--loc];
-				yield return new KeyValuePair<TKey, TValue>(current.Key, current.Value);
-
-				current = current.Right;
-				FillStack(stack, ref loc, current);
-			}
-		}
-
-		private void FillStack(Node[] stack, ref int index, Node current)
-		{
-			while (current != null)
-			{
-				stack[index++] = current;
-				current = current.Left;
-			}
-		}
-
-		IEnumerator IEnumerable.GetEnumerator()
-		{
-			return this.GetEnumerator();
-		}
 		#endregion
 
 
-		#region Helper Classes
-		/// <summary>
-		/// A recursive DFS post-order algorithm for clearing the tree.
-		/// </summary>
-		private void Clear(Node n)
-		{
-			if (n == null)
-				return;
-
-			Clear(n.Left);
-			Clear(n.Right);
-
-			n.Left = null;
-			n.Right = null;
-			n.Parent = null;
-		}
+		#region Helper Methods
 
 		/// <summary>
-		/// Returns the parent node of the node that would contain
-		/// the given key. Only the root node had no parent. If the
-		/// node is not in the tree, then the given parent is the
-		/// location where the node should be inserted.
+		/// Returns the parent node of the node that would contain the given 
+		/// key. Only the root node had no parent. If the node is not in the 
+		/// tree, then the given parent is the location where the node should 
+		/// be inserted.
 		/// </summary>
-		/// <param name="key"></param>
-		/// <returns></returns>
 		private Node GetParent(TKey key)
 		{
 			if (Count == 0)
@@ -323,12 +219,11 @@ namespace Unvi.DataStructures.Dictionaries
 			}
 
 			return parent;
-		}
-
+		}	// End -GetParent(key: TKey): Node
+		
 		/// <summary>
-		/// Using the given 'node' as a starting point, this method
-		/// will move up the tree re-balancing it based on an AVL
-		/// node rotation algorithm.
+		/// Using the given 'node' as a starting point, this method will move 
+		/// up the tree re-balancing it based on an AVL node rotation algorithm.
 		/// </summary>
 		private void RebalanceTree(Node node)
 		{
@@ -357,13 +252,13 @@ namespace Unvi.DataStructures.Dictionaries
 
 				node = node.Parent;
 			}
-		}
+		}	// End -RebalanceTree(node: Node)
 
 		/// <summary>
-		/// Rotates the given node down into its left child, while rotating
-		/// the right child of the given node into its current position in
-		/// the tree. This method requires that the given node contains a right
-		/// child in order to function.
+		/// Rotates the given node down into its left child, while rotating the 
+		/// right child of the given node into its current position in the tree. 
+		/// This method requires that the given node contains a right child in 
+		/// order to function.
 		/// </summary>
 		/// <returns>
 		/// Returns the node that has taken the tree position of the given node.
@@ -395,12 +290,12 @@ namespace Unvi.DataStructures.Dictionaries
 			// having its height updated.
 
 			return replacement;
-		}
+		}	// End -RotateLeft(node: Node)
 
 		/// <summary>
-		/// This node will be rotated down into the position of its right child while
-		/// its left child is rotated into its current position. A left child is required
-		/// for the method to function.
+		/// This node will be rotated down into the position of its right child 
+		/// while its left child is rotated into its current position. A left 
+		/// child is required for the method to function.
 		/// </summary>
 		/// <returns>
 		/// Returns the node that has taken the tree position of the given node.
@@ -430,11 +325,223 @@ namespace Unvi.DataStructures.Dictionaries
 			replacement.UpdateHeight();
 
 			return replacement;
-		}
+		}	// End -RotateRight(node: Node)
+
+		/// <summary>
+		/// This method will remove the node with the given key from the tree. It will then
+		/// return the node that is located at the spot of the tree where the tree needs to
+		/// be re-balanced. It will also keep the tree's node count up to date.
+		/// </summary>
+		private Node RemoveNode(TKey key)
+		{
+			// ToDo: Extract finding the replacement node into its own method.
+			Node parent;
+			Node node;
+			Node replacement;
+
+			parent = GetParent(key);
+			if (parent == null)
+				node = _root;
+			else if (key.CompareTo(parent.Key) < 0)
+				node = parent.Left;
+			else
+				node = parent.Right;
+
+			// Special case. The replacement node should be the rightmost node
+			// of the left sub-tree of the node that will be removed from the
+			// tree. If we don't have a replacement node on the left subtree,
+			// we will use the first node on the right subtree.
+			if (node.Left == null)
+			{
+				replacement = node.Right;
+
+				if (parent == null)
+					_root = replacement;
+				else if (key.CompareTo(parent.Key) < 0)
+					parent.Left = replacement;
+				else
+					parent.Right = replacement;
+
+				if (replacement != null)
+					replacement.Parent = parent;
+
+				node.Parent = null;
+				node.Right = null;
+				Count--;
+
+				return replacement;
+			}
+
+			replacement = node.Left;
+			while (replacement.Right != null)
+				replacement = replacement.Right;
+
+			Node rebalancePoint = null;
+
+			if (replacement == node.Left)
+			{
+				rebalancePoint = replacement;
+			}
+			else
+			{
+				rebalancePoint = replacement.Parent;
+
+				// The replacement was the right most child of the sub-tree, so we
+				// know that it resides on the right side of its parent.
+				replacement.Right = replacement.Left;
+				if (replacement.Right != null)
+					rebalancePoint.Right.Parent = replacement;
+
+				replacement.Left = node.Left;
+				replacement.Left.Parent = replacement;
+			}
+
+			replacement.Right = node.Right;
+			if (replacement.Right != null)
+				replacement.Right.Parent = replacement;
+
+			replacement.Parent = parent;
+			if (parent == null)
+				_root = replacement;
+			else if (key.CompareTo(parent.Key) < 0)
+				parent.Left = replacement;
+			else
+				parent.Right = replacement;
+
+			node.Left = null;
+			node.Right = null;
+			node.Parent = null;
+
+			Count--;
+			return rebalancePoint;
+		}	// End -RemoveNode(key: TKey): Node
+
+		/// <summary>
+		/// A recursive DFS post-order algorithm for clearing the tree.
+		/// </summary>
+		private void Clear(Node node)
+		{
+			if (node == null)
+				return;
+
+			Clear(node.Left);
+			Clear(node.Right);
+
+			node.Left	= null;
+			node.Right	= null;
+			node.Parent = null;
+		}	// End -Clear(node: Node)
 		#endregion
 
 
-		#region Inner Classes
+		public bool ContainsKey(TKey key)
+		{
+			throw new NotImplementedException();
+		}
+
+		public ICollection<TKey> Keys
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public bool TryGetValue(TKey key, out TValue value)
+		{
+			throw new NotImplementedException();
+		}
+
+		public ICollection<TValue> Values
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public bool Contains(KeyValuePair<TKey, TValue> item)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool IsReadOnly
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+		{
+			throw new NotImplementedException();
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			throw new NotImplementedException();
+		}
+
+
+
+		//#region Public Methods
+
+		///// <summary>
+		///// Checks to see if there's an entry under the given key.
+		///// </summary>
+		//public bool ContainsKey(TKey key)
+		//{
+		//	if(key == null)
+		//		return false;
+
+		//	if (Count == 0)
+		//		return false;
+
+		//	var parent = GetParent(key);
+
+		//	if (parent == null)
+		//		return _root.Key.CompareTo(key) == 0;
+
+		//	var node = key.CompareTo(parent.Key) < 0 ? parent.Left : parent.Right;
+
+		//	return node != null;
+		//}
+
+		//public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+		//{
+		//	if (_root == null)
+		//		yield break;
+
+		//	var stack = new Node[_root.Height];
+		//	int loc = 0;
+		//	var current = _root;
+
+		//	FillStack(stack, ref loc, current);
+
+		//	while (loc > 0)
+		//	{
+		//		current = stack[--loc];
+		//		yield return new KeyValuePair<TKey, TValue>(current.Key, current.Value);
+
+		//		current = current.Right;
+		//		FillStack(stack, ref loc, current);
+		//	}
+		//}
+
+		//private void FillStack(Node[] stack, ref int index, Node current)
+		//{
+		//	while (current != null)
+		//	{
+		//		stack[index++] = current;
+		//		current = current.Left;
+		//	}
+		//}
+
+		//IEnumerator IEnumerable.GetEnumerator()
+		//{
+		//	return this.GetEnumerator();
+		//}
+		//#endregion
+
+
+		#region Helper Classes
 		private class Node
 		{
 			#region Properties
@@ -445,12 +552,12 @@ namespace Unvi.DataStructures.Dictionaries
 			public TKey		Key		{ get; private set; }
 			public TValue	Value	{ get; set; }
 
-			public int		Height { get; private set; }
-			public int		Balance
+			public int Height { get; private set; }
+			public int Balance
 			{
 				get
 				{
-					int left = Left != null ? Left.Height : 0;
+					int left  = Left  != null ? Left.Height  : 0;
 					int right = Right != null ? Right.Height : 0;
 
 					return left - right;
@@ -462,13 +569,13 @@ namespace Unvi.DataStructures.Dictionaries
 			#region Constructors
 			public Node(TKey key, TValue value)
 			{
-				Key		= key;
-				Value	= value;
-				Height	= 1;
+				Key = key;
+				Value = value;
+				Height = 1;
 
-				Parent	= null;
-				Left	= null;
-				Right	= null;
+				Parent = null;
+				Left = null;
+				Right = null;
 			}
 			#endregion
 
