d319e3ba1bf8560ad00c915a9a1940ba3cb0403c
Francisco Chavez-Tejeda <francisco.chavez.509@outlook.com>
2015-11-27 16:08:48 -0800

    Created method stubs for PriorityQueue's interface.

--- Code/DataStructures/DataStructures/PriorityQueue.cs
+++ Code/DataStructures/DataStructures/PriorityQueue.cs
@@ -4,9 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Unvi.DataStructures
 {
-	public class PriorityQueue
+	public class PriorityQueue<T>
 	{
+		public T Dequeue { get { throw new NotImplementedException(); } }
+		public T Pop { get { throw new NotImplementedException(); } }
+
+		public T Peek { get { throw new NotImplementedException(); } }
+
+		public int Count { get; private set; }
+
+
+		public void Enqueue(T value, int priority = int.MinValue) { throw new NotImplementedException(); }
+		public void Push(T value, int priority = int.MinValue) { throw new NotImplementedException(); }
 	}
 }
