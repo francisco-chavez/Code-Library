775433379df8357a43ab505bcbb11ccad43d88fe
Francisco Chavez-Tejeda <francisco.chavez.509@outlook.com>
2015-11-27 16:12:10 -0800

    Adding constructors.

--- Code/DataStructures/DataStructures/PriorityQueue.cs
+++ Code/DataStructures/DataStructures/PriorityQueue.cs
@@ -10,11 +10,33 @@ namespace Unvi.DataStructures
 	public class PriorityQueue<T>
 	{
 		public T Dequeue { get { throw new NotImplementedException(); } }
-		public T Pop { get { throw new NotImplementedException(); } }
+		public T Pop { get { return Dequeue; } }
 
 		public T Peek { get { throw new NotImplementedException(); } }
 
 		public int Count { get; private set; }
+
+
+		#region Constructors
+		public PriorityQueue()
+		{
+			throw new NotImplementedException();
+		}
+
+		public PriorityQueue(IEnumerable<T> values, int priority = int.MinValue)
+		{
+			throw new NotImplementedException();
+		}
+
+		public PriorityQueue(IEnumerable<IEnumerable<T>> values, int startingPriority = int.MinValue)
+		{
+			throw new NotImplementedException();
+		}
+
+		~PriorityQueue()
+		{
+		}
+		#endregion
 
 
 		public void Enqueue(T value, int priority = int.MinValue) { throw new NotImplementedException(); }
