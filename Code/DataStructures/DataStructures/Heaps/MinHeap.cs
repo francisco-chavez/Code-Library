2d34e012569e28284138e8b20d6b2b1ac8f52971
Francisco Chavez-Tejeda <francisco.chavez.509@outlook.com>
2015-11-30 00:14:11 -0800

    Filled in the implementations for MinHeap.

--- Code/DataStructures/DataStructures/Heaps/MinHeap.cs
+++ Code/DataStructures/DataStructures/Heaps/MinHeap.cs
@@ -12,17 +12,30 @@ namespace Unvi.DataStructures.Heaps
 	{
 		public override HeapType HeapType
 		{
-			get { throw new NotImplementedException(); }
+			get { return DataStructures.HeapType.Min; }
 		}
 
 		protected override bool HeapValidFor(int parent, int child)
 		{
-			throw new NotImplementedException();
+			if (child >= Count)
+				return true;
+
+			return _data[parent].CompareTo(_data[child]) <= 0;
 		}
 
+		/// <summary>
+		/// We only enter this method when the value in parent is greater than the value in left
+		/// and/or the value in right. This method will return the child with the lowest value
+		/// because that value will do the best job at maintaining the heap when it becomes
+		/// the parent value.
+		/// </summary>
+		/// <param name="parent">A value geater than or equal to 0 and less than left.</param>
+		/// <param name="left">A value equal to parent time two plus 1 and less than Count.</param>
+		/// <param name="right">A value equal to left + 1 and less than Count.</param>
+		/// <returns>The index that holds the minimum value.</returns>
 		protected override int GetBestParent(int parent, int left, int right)
 		{
-			throw new NotImplementedException();
+			return _data[left].CompareTo(_data[right]) <= 0 ? left : right;
 		}
 	}
 }
