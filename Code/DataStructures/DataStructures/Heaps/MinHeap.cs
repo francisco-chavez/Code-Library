953d32d73d15199336a7f86bb3909df32968b775
Francisco Chavez-Tejeda <francisco.chavez.509@outlook.com>
2015-11-29 23:13:31 -0800

    I'm really not liking the extra code that used to have a Heap that can be either a Min or Max Heap.

---
+++ Code/DataStructures/DataStructures/Heaps/MinHeap.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Unvi.DataStructures.Heaps
+{
+	public class MinHeap<T>
+		: IHeap<T> where T: IComparable<T>
+	{
+		#region Attributes
+		#endregion
+
+
+		#region Properties
+
+		public HeapType HeapType
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public T Peek
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public int Count
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public bool IsEmpty
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		#endregion
+
+
+		#region Constructors
+		#endregion
+
+
+		#region Public Methods
+
+		public void Push(T value)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Clear()
+		{
+			throw new NotImplementedException();
+		}
+
+		public T Pop()
+		{
+			throw new NotImplementedException();
+		}
+
+		#endregion
+
+	}
+}
